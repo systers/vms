dc33dd006964e70f4c445718b6363109870331e7
KokareIITP <pmkokare97@gmail.com>
2016-02-21 02:31:04 +0530

    Edit job to lie within event dates

--- vms/job/views.py
+++ vms/job/views.py
@@ -76,7 +76,6 @@ def create(request):
         return render(request, 'vms/no_admin_rights.html')
 
 
-
 @login_required
 def delete(request, job_id):
     if is_admin(request):
@@ -122,23 +121,16 @@ def edit(request, job_id):
                 job_to_edit = form.save(commit=False)
                 event_id = request.POST.get('event_id')
                 event = get_event_by_id(event_id)
-                if event:
-                    job_to_edit.event = event
-                else:
-                    raise Http404
-                job_to_edit.save()
-                
-		start_date_event=event.start_date
+                start_date_event =event.start_date
                 end_date_event=event.end_date
                 start_date_job=form.cleaned_data.get('start_date')
                 end_date_job=form.cleaned_data.get('end_date')
                 if(start_date_job>=start_date_event and end_date_job<=end_date_event):
-                    job = form.save(commit=False)
                     if event:
-                        job.event = event
+                        job_to_edit.event = event
                     else:
                         raise Http404
-                    job.save()
+                    job_to_edit.save()
                     return HttpResponseRedirect(reverse('job:list'))
                 else:
                     messages.add_message(request, messages.INFO, 'Job dates should lie within Event dates')
@@ -147,7 +139,6 @@ def edit(request, job_id):
                     'job/edit.html',
                     {'form': form, 'event_list': event_list , 'job': job}
                     )
-		return HttpResponseRedirect(reverse('job:list'))
 
             else:
                 return render(
