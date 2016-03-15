71ba236f4f035e4c172ba88eab000d02fd1d8011
vatsala <vatsala.swaroop@gmail.com>
2016-03-15 14:44:44 +0530

    Dealing with skipped fields in location

--- vms/event/validate_address.py
+++ vms/event/validate_address.py
@@ -2,6 +2,8 @@ from easy_maps import geocode
 
 def validate_address(location):
     if not location.isspace():
+    	#removing extra spaces
+    	location = " ".join(location.split())
         try:
         	location = geocode.google_v3(location)
         except:
