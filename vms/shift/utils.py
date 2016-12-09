a2621fe5baa57cd1b87c9c28fac28b637709c840
Anh3h <courageangeh@gmail.com>
2016-12-09 10:32:38 +0100

    Fix issue on about page

--- vms/shift/utils.py
+++ vms/shift/utils.py
@@ -234,16 +234,3 @@ def log_hours_utility():
         start_time = '09:00',
         end_time = '12:00'
         )
-
-def send_twilio_message(to_number, body):
-    """
-    Sends sms to a number
-    """
-    client = twilio.rest.TwilioRestClient(
-        settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
- 
-    return client.messages.create(
-        body=body,
-        to=to_number,
-        from_=settings.TWILIO_PHONE_NUMBER
-    )
