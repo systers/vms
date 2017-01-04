870ec693aeb6edc27cf6cd306114dbe81c32d01b
InfernoCoder <sohamsen1000@gmail.com>
2017-01-04 20:21:32 +0530

    Added functionality to prevent event duplicates

--- vms/event/forms.py
+++ vms/event/forms.py
@@ -22,11 +22,17 @@ class EventForm(ModelForm):
     def clean(self):
         start_date = self.cleaned_data.get('start_date')
         end_date = self.cleaned_data.get('end_date')
+        event_name = self.cleaned_data.get('name')
 
         if start_date and end_date:
             if start_date > end_date:
                 msg = u"Start date must be before the end date"
                 self._errors['start_date'] = self.error_class([msg])
+
+        for event in Event.objects.all():
+            if event_name == event.name:
+                self._errors['name'] = self.error_class(
+                    [u"Another event with same name already exists"])
 
         return self.cleaned_data
 
