a1daa4eadb0a17fa482a6731a236fbceed82c0a1
Soham Sen <sohamsen1000@gmail.com>
2017-01-27 16:17:25 +0530

    Fixed tests

--- vms/shift/tests/test_viewVolunteerShift.py
+++ vms/shift/tests/test_viewVolunteerShift.py
@@ -68,7 +68,6 @@ class ViewVolunteerShift(LiveServerTestCase):
         upcoming_shift_page = self.upcoming_shift_page
         upcoming_shift_page.get_page(self.live_server_url, upcoming_shift_page.view_shift_page + '65459')
         found = re.search('You don\'t have the required rights', self.driver.page_source)
-        found = re.search('You don\'t have the necessary rights to access this page', self.driver.page_source)
         self.assertNotEqual(found, None)
 
     def test_view_without_any_assigned_shift(self):
