4b8481d81a914fc296e2f50dc72bfe5f3db0bcfd
anjali-dhanuka <anjalidhanuka99@gmail.com>
2017-12-24 22:37:59 +0530

    password change functionality added
    password chnage form indented
    
    changes implemented in templates

--- vms/authentication/urls.py
+++ vms/authentication/urls.py
@@ -15,5 +15,10 @@ urlpatterns = [
     url(r'^logout/$',
         LogoutView.as_view(template_name='home/home.html'),
         name='logout_process'),
+    url(r'^change-password/$',
+        auth_views.password_change, {'post_change_redirect': 'done/'},
+        name='password_change'),
+    url(r'^change-password/done/$',
+        auth_views.password_change_done,
+        name='password_change_done'),
 ]
-
