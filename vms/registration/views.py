4e25ea6847b1a558660469b67761665e3e8290d0
anjali-dhanuka <anjalidhanuka99@gmail.com>
2017-12-25 05:57:17 +0530

    email confirmation on sign up functionality added

--- vms/registration/views.py
+++ vms/registration/views.py
@@ -1,12 +1,20 @@
 # third party
 
 # Django
+from django.http import HttpResponse
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
+from django.contrib.sites.models import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
 
 # local Django
 from administrator.forms import AdministratorForm
@@ -18,7 +26,7 @@ from registration.phone_validate import validate_phone
 from registration.utils import volunteer_denied
 from volunteer.forms import VolunteerForm
 from volunteer.validation import validate_file
-
+#import pdb
 
 class AdministratorSignupView(TemplateView):
     """
@@ -139,7 +147,7 @@ class VolunteerSignupView(TemplateView):
                 if user_form.is_valid() and volunteer_form.is_valid():
 
                     vol_country = request.POST.get('vol-country')
-                    vol_phone = request.POST.get('vol-phone_number')
+                    vol_phone = request.POST.get('vol-phone_number') 
                     if (vol_country and vol_phone):
                         if not validate_phone(vol_country, vol_phone):
                             self.phone_error = True
@@ -183,14 +191,24 @@ class VolunteerSignupView(TemplateView):
 
                     if organization:
                         volunteer.organization = organization
-
+                    
                     volunteer.reminder_days = 1
                     volunteer.save()
-                    registered = True
-
-                    messages.success(request,
-                                     'You have successfully registered!')
-                    return HttpResponseRedirect(reverse('home:index'))
+                    user.is_active = False                    
+                    current_site = get_current_site(request)
+                    mail_subject = 'Activate your blog account.'
+                    message = render_to_string('registration/acc_active_email.html', {
+                        'user': user,
+                        'domain': current_site.domain,
+                        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                        'token':account_activation_token.make_token(user),
+                    })
+                    to_email = volunteer_form.cleaned_data.get('email')
+                    email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+                     )
+                    email.send()
+                    return HttpResponse('Please confirm your email address to complete the registration')
                 else:
                     print(user_form.errors, volunteer_form.errors)
                     return render(
@@ -204,3 +222,15 @@ class VolunteerSignupView(TemplateView):
         else:
             return render(request, 'home/home.html', {'error': True})
 
+def activate(request, uidb64, token):
+   try:
+      uid = force_text(urlsafe_base64_decode(uidb64))
+      user = User.objects.get(pk=uid)
+   except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+      user = None
+   if user is not None and account_activation_token.check_token(user, token):
+      user.is_active = True
+      user.save()
+      return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+   else:
+      return HttpResponse('Activation link is invalid!')
