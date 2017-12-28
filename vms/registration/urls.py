f64a1858806b1098016efb6eece6ad168ada09fd
anjali-dhanuka <anjalidhanuka99@gmail.com>
2017-12-28 17:12:34 +0530

    registration cleaned

--- vms/registration/urls.py
+++ vms/registration/urls.py
@@ -6,7 +6,12 @@ from administrator.views import *
 from registration import views
 from registration.views import *
 
-urlpatterns = patterns('',
-    url(r'^signup_administrator/$', AdministratorSignupView.as_view(), name='signup_administrator'),
-    url(r'^signup_volunteer/$', VolunteerSignupView.as_view(), name='signup_volunteer'),
+urlpatterns = patterns(
+    '',
+    url(r'^signup_administrator/$',
+        AdministratorSignupView.as_view(),
+        name='signup_administrator'),
+    url(r'^signup_volunteer/$',
+        VolunteerSignupView.as_view(),
+        name='signup_volunteer'),
 )
