cd81669ded752f2258b0218c746ee5c97d4f5c0a
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-01-14 06:34:13 +0530

    commented code cleaned and debug added in options of templates

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -16,8 +16,6 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: run with debug turned off (DEBUG = False) in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -70,6 +68,8 @@ DATABASES = {
     }
 }
 
+
+
 TEMPLATES = [
     {
        'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,14 +81,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'django.contrib.auth.context_processors.auth',
-                # 'django.core.context_processors.debug',
-                 #'django.core.context_processors.i18n',
-                # 'django.core.context_processors.media',
-                # 'django.core.context_processors.static',
-                # 'django.core.context_processors.tz',
-                # 'django.contrib.messages.context_processors.messages',
                 ],
+            'debug': DEBUG
             },
          },
 ]
