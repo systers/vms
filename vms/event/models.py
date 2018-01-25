ef548e0798e593e416bb0391c30a5ed9a426e69f
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-01-25 18:50:35 +0530

    all merge conflicts resolved

--- vms/event/models.py
+++ vms/event/models.py
@@ -24,39 +24,9 @@ class Event(models.Model):
         blank=True,
         null=True,
     )
-<<<<<<< 244758948f038652171e45bf3da919d73da4a355
-    city = models.CharField(
-        max_length=75,
-        validators=[
-            RegexValidator(r'^[(A-Z)|(a-z)|(\s)|(\-)|(\')]+$', ),
-        ],
-        blank=True,
-        null=True,
-    )
-    state = models.CharField(
-        max_length=50,
-        validators=[
-            RegexValidator(r'^[(A-Z)|(a-z)|(\s)|(\-)]+$', ),
-        ],
-        blank=True,
-        null=True,
-    )
-    country = models.CharField(
-        max_length=75,
-        validators=[
-            RegexValidator(r'^[(A-Z)|(a-z)|(\s)|(\-)|(\')]+$', ),
-        ],
-        blank=True,
-        null=True,
-    )
-=======
     city = models.ForeignKey(City)
-
     state = models.ForeignKey(Region)
-
     country = models.ForeignKey(Country)
->>>>>>> drop downs added in admin view
-
     venue = models.CharField(
         max_length=30,
         validators=[
