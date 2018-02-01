606a053e2f321d095099bfd212a213c41b42510e
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-02-01 13:28:44 +0530

    manage.py modified

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
