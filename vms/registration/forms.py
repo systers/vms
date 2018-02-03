2bf3a41f58ca92da176920be8332735547761ab3
sammy1997 <sombuddha2016@gmail.com>
2018-02-03 19:19:17 +0530

    Added length check on password field

--- vms/registration/forms.py
+++ vms/registration/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
 class UserForm(forms.ModelForm):
-    #password not visible when user types it out
-    password = forms.CharField(widget=forms.PasswordInput())
+    # password not visible when user types it out
+    password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=25)
 
     class Meta:
         model = User
