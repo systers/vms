648cb050e3409a4ee0bf02e80c764107127de084
sammy1997 <sombuddha2016@gmail.com>
2018-02-06 18:02:22 +0530

    Added __unicode__ functions to all models so as to increase transparency in django-admin

--- vms/organization/models.py
+++ vms/organization/models.py
@@ -12,3 +12,6 @@ class Organization(models.Model):
             RegexValidator(r'^[(A-Z)|(a-z)|(0-9)|(\s)|(\-)|(:)|(\')]+$', ),
         ],
     )
+
+    def __unicode__(self):
+        return self.name
