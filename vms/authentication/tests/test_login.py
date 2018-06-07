d1e87895358ebb0710ed9916afc8d417906ab80b
Monal5031 <monalshadi16@gmail.com>
2018-06-07 12:14:08 +0530

    Testing Authentication app

--- vms/authentication/tests/test_login.py
+++ vms/authentication/tests/test_login.py
@@ -1,41 +1,42 @@
 # third party
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Django
 from django.contrib.staticfiles.testing import LiveServerTestCase
 
 # local Django
 from pom.pages.authenticationPage import AuthenticationPage
-
+from pom.locators.authenticationPageLocators import AuthenticationPageLocators
 from shift.utils import (create_admin, create_volunteer)
 
 
-# Class contains failing test cases which have been documented
-# Test class commented out to prevent travis build failure
-"""
 class TestAccessControl(LiveServerTestCase):
-    '''
+    """
     TestAccessControl class contains the functional tests to check Admin and
     Volunteer can access '/home' view of VMS. Following tests are included:
     Administrator:
         - Login admin with correct credentials
-        - Login admin with incorrect credentials 
+        - Login admin with incorrect credentials
     Volunteer:
         - Login volunteer with correct credentials
-        - Login volunteer with incorrect credentials 
-    '''
+        - Login volunteer with incorrect credentials
+    """
 
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
         cls.driver.maximize_window()
         cls.authentication_page = AuthenticationPage(cls.driver)
+        cls.wait = WebDriverWait(cls.driver, 5)
         super(TestAccessControl, cls).setUpClass()
 
     def setUp(self):
-        admin = create_admin()
-        volunteer = create_volunteer()
+        create_admin()
+        create_volunteer()
 
     def tearDown(self):
         pass
@@ -45,79 +46,135 @@ class TestAccessControl(LiveServerTestCase):
         cls.driver.quit()
         super(TestAccessControl, cls).tearDownClass()
 
+    def login(self, username, password):
+        self.authentication_page.login({
+            'username': username,
+            'password': password
+        })
+
     def test_correct_admin_credentials(self):
-        '''
+        """
         Method to simulate logging in of a valid admin user and check if they
         redirected to '/home' and no errors are generated.
-        '''
+        """
         authentication_page = self.authentication_page
-        self.authentication_page.server_url = self.live_server_url
-        authentication_page.login({'username': 'admin', 'password': 'admin'})
-        self.assertEqual(self.driver.current_url,
+        authentication_page.server_url = self.live_server_url
+        authentication_page.go_to_authentication_page()
+        username = password = 'admin'
+        self.login(username, password)
+
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH,
+                 "//h1[contains(text(), 'Volunteer Management System')]"))
+        )
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
                          self.live_server_url + authentication_page.homepage)
 
-        with self.assertRaises(NoSuchElementException):
-            authentication_page.get_incorrect_login_message()
+        self.assertRaisesRegexp(NoSuchElementException,
+                                'Message: Unable to locate element: .alert-danger',
+                                authentication_page.get_incorrect_login_message)
         authentication_page.logout()
 
     def test_incorrect_admin_credentials(self):
-        '''
+        """
         Method to simulate logging in of an Invalid admin user and check if
         they are displayed an error and redirected to login page again.
-        '''
+        """
         authentication_page = self.authentication_page
-        self.authentication_page.server_url = self.live_server_url
-        authentication_page.login({
-            'username': 'admin',
-            'password': 'wrong_password'
-        })
-        self.assertNotEqual(
-            self.driver.current_url,
-            self.live_server_url + authentication_page.homepage)
+        authentication_page.server_url = self.live_server_url
+        authentication_page.go_to_authentication_page()
+        username = 'admin'
+        password = 'wrong_password'
+        self.login(username, password)
 
-        self.assertEqual(self.driver.current_url,
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, '.' + AuthenticationPageLocators.INCORRECT_LOGIN_ERROR))
+        )
+
+        self.assertNotEqual(authentication_page.remove_i18n(self.driver.current_url),
+                            self.live_server_url + authentication_page.homepage)
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
                          self.live_server_url + authentication_page.url)
 
         self.assertNotEqual(authentication_page.get_incorrect_login_message(),
                             None)
 
     def test_correct_volunteer_credentials(self):
-        '''
+        """
         Method to simulate logging in of a valid volunteer user and check if
         they are redirected to '/home'
-        '''
+        """
         authentication_page = self.authentication_page
-        self.authentication_page.server_url = self.live_server_url
-        authentication_page.login({
-            'username': 'volunteer',
-            'password': 'volunteer'
-        })
-        self.assertEqual(self.driver.current_url,
+        authentication_page.server_url = self.live_server_url
+        authentication_page.go_to_authentication_page()
+        username = password = 'volunteer'
+        self.login(username, password)
+
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH,
+                 "//h1[contains(text(), 'Volunteer Management System')]"))
+        )
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
                          self.live_server_url + authentication_page.homepage)
 
-        with self.assertRaises(NoSuchElementException):
-            authentication_page.get_incorrect_login_message()
+        self.assertRaisesRegexp(NoSuchElementException,
+                                'Message: Unable to locate element: .alert-danger',
+                                authentication_page.get_incorrect_login_message)
         authentication_page.logout()
 
     def test_incorrect_volunteer_credentials(self):
-        '''
-        Method to simulate logging in of a Invalid volunteer user and check if
+        """
+        Method to simulate logging in of an invalid volunteer user and check if
         they are displayed an error and redirected to login page again.
-        '''
+        """
         authentication_page = self.authentication_page
-        self.authentication_page.server_url = self.live_server_url
-        authentication_page.login({
-            'username': 'volunteer',
-            'password': 'wrong_password'
-        })
+        authentication_page.server_url = self.live_server_url
+        authentication_page.go_to_authentication_page()
+        username = 'volunteer'
+        password = 'wrong_password'
+        self.login(username, password)
 
-        self.assertNotEqual(
-            self.driver.current_url,
-            self.live_server_url + authentication_page.homepage)
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, '.' + AuthenticationPageLocators.INCORRECT_LOGIN_ERROR))
+        )
 
-        self.assertEqual(self.driver.current_url,
+        self.assertNotEqual(authentication_page.remove_i18n(self.driver.current_url),
+                            self.live_server_url + authentication_page.homepage)
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
                          self.live_server_url + authentication_page.url)
-
-<<<<<<< ec806c6d047fd408e0fd890e7c6845e2f54622e6
         self.assertNotEqual(authentication_page.get_incorrect_login_message(), None)
-"""
+
+    def test_login_page_after_authentication(self):
+        authentication_page = self.authentication_page
+        authentication_page.server_url = self.live_server_url
+        username = password = 'admin'
+        self.login(username, password)
+
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH,
+                 "//h1[contains(text(), 'Volunteer Management System')]"))
+        )
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
+                         self.live_server_url + authentication_page.homepage)
+
+        self.assertRaisesRegexp(NoSuchElementException,
+                                'Message: Unable to locate element: .alert-danger',
+                                authentication_page.get_incorrect_login_message)
+
+        authentication_page.get_page(authentication_page.server_url, '/authentication/')
+
+        self.assertEqual(authentication_page.remove_i18n(self.driver.current_url),
+                         self.live_server_url + authentication_page.homepage)
+
+        authentication_page.logout()
+
