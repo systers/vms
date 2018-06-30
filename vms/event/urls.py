e7e399c9e49e77ad8883622d87dde3170d6e6fb1
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-07-01 00:36:46 +0530

    detail view added for events

--- vms/event/urls.py
+++ vms/event/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import url
 
 # local Django
 from event import views
-from event.views import EventCreateView, EventDeleteView, EventUpdateView, EventListView
+from event.views import EventCreateView, EventDeleteView, EventDetailView, EventUpdateView, EventListView
 
 urlpatterns = [
     url(r'^create/$', EventCreateView.as_view(), name='create'),
     url(r'^delete/(?P<event_id>\d+)$',
         EventDeleteView.as_view(),
-        name='delete'),
+        name='delete'),   
+    url(r'^details/(?P<event_id>\d+)$', EventDetailView.as_view(), name='details'),
     url(r'^edit/(?P<event_id>\d+)$', EventUpdateView.as_view(), name='edit'),
     url(r'^list/$', EventListView.as_view(), name='list'),
     url(r'^list_sign_up/(?P<volunteer_id>\d+)$',
