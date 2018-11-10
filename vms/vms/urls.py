ce2703f8beefaffb976deb77d4c48d009fe57648
Tlazypanda <mishrasneha.bb@gmail.com>
2018-11-10 19:29:23 +0530

    Remove redclaration of home url

--- vms/vms/urls.py
+++ vms/vms/urls.py
@@ -12,7 +12,6 @@ urlpatterns = i18n_patterns(
     url(r'^authentication/',
         include('authentication.urls', namespace='authentication')),
     url(r'^event/', include('event.urls', namespace='event')),
-    url(r'^home/', include('home.urls', namespace='home')),
     url(r'^job/', include('job.urls', namespace='job')),
     url(r'^organization/',
         include('organization.urls', namespace='organization')),
@@ -21,4 +20,3 @@ urlpatterns = i18n_patterns(
     url(r'^shift/', include('shift.urls', namespace='shift')),
     url(r'^volunteer/', include('volunteer.urls', namespace='volunteer')),
 )
-
