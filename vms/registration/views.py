4434224975c42c5474fca133cb67963c9025e197
khatryshikha <shikhakhatry2612@gmail.com>
2018-12-12 20:25:58 +0530

    Flake8 error, changes of more than 80 words in a line and Indentation correction

--- vms/registration/views.py
+++ vms/registration/views.py
@@ -102,7 +102,8 @@ class AdministratorSignupView(TemplateView):
 
                 try:
                     admin_city_name = request.POST.get('city')
-                    admin_city = City.objects.get(name=admin_city_name, region=admin_state)
+                    admin_city = City.objects.get(name=admin_city_name,
+                                                  region=admin_state)
                 except ObjectDoesNotExist:
                     admin_city = None
 
