7f5282cb2eb1f3ad7a36d5ab65db66e3db60c8ec
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-08 17:26:26 +0530

    Add Allowed host setting through .env

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: run with debug turned off (DEBUG = False) in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 INSTALLED_APPS = (
