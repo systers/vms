be19026be48245d13d0f625fb0cfb4f862100eb9
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-03-15 15:35:31 +0530

    exporting key ::bug

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -13,6 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+# export DJANGO_SECRET_KEY='foobarbaz'
+
 # SECURITY WARNING: run with debug turned off (DEBUG = False) in production!
 DEBUG = True
 
