c2a846a7f2ef0b48c545102e7a86ecfd290835b0
Anwesha Sen <anwesha.sen321@gmail.com>
2020-03-21 20:37:57 +0530

    Add check for pdf validation

--- vms/volunteer/validation.py
+++ vms/volunteer/validation.py
@@ -1,3 +1,9 @@
+from PyPDF2 import PdfFileReader
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from vms.settings import MEDIA_ROOT
+import os.path
+
 def validate_file(my_file):
 
     MAX_FILENAME_LENGTH = 40
@@ -17,6 +23,16 @@ def validate_file(my_file):
         is_valid = False
     if my_file.size > MAX_FILESIZE_BYTES:
         is_valid = False
+    else:
+        if my_file.content_type == "application/pdf":
+            path = default_storage.save(my_file.name, ContentFile(my_file.read()))
+            path = os.path.join(MEDIA_ROOT, path)
+            try:
+                PdfFileReader(open(path, 'rb'))
+            except Exception as e:
+                print("Read failed or invalid: ", e)
+                is_valid = False
+            default_storage.delete(my_file.name)
     if my_file.content_type not in VALID_CONTENT_TYPES:
         is_valid = False
 
