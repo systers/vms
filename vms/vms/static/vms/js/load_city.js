82227f40f223fccc175f2d23e90b1db8626e165c
Prerna Singh <51945896+psingh587@users.noreply.github.com>
2020-09-08 14:26:09 +0530

    Disables the city input field until the state input is filled up

--- vms/vms/static/vms/js/load_city.js
+++ vms/vms/static/vms/js/load_city.js
@@ -1,6 +1,18 @@
-/** Fetches cities belonging to selected state and country */
 $(document).ready(function() {
     $("#select_state").change(function() {
+/* Disables the city input field until the state input is filled up */
+	function hideCity(){
+		var state =document.getElementById("select_state");
+		if(state.disabled===false && state.value!==null && state.value!=="0") {
+			document.getElementById("select_city").disabled=false;
+		}
+		else {
+			document.getElementById("select_city").disabled=true;
+		}
+	}
+    hideCity(); 
+
+/** Fetches cities belonging to selected state and country */
         var countryId = $("#select_country").val();
         var stateId = $(this).val();
         $.ajax({
@@ -10,8 +22,16 @@ $(document).ready(function() {
                 "state": stateId
             },
             success: function(cities) {
-                $("#select_city").html(cities);
+		var state=$("#select_state"); 
+		if(state.val()!=="0" && state.val()!==null && state.prop('disabled')===false)
+                	$("#select_city").html(cities);
+		else  
+			$("#select_city").empty();
             }
         });
+
+
     });
+
 });
+
