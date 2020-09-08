6ade48b7de6b3af715f55dd6f453f8c87cad6247
Prerna Singh <51945896+psingh587@users.noreply.github.com>
2020-09-08 14:28:48 +0530

    Disables the city and state input field until the country input is filled up
    Handled All Scenarios

--- vms/vms/static/vms/js/load_state.js
+++ vms/vms/static/vms/js/load_state.js
@@ -1,6 +1,27 @@
-/** Fetches states belonging to selected  country */
 $(document).ready(function() {
     $("#select_country").change(function() {
+/* Disables the city and state input field until the country input is filled up */
+	function hideState(){
+ 		var country =document.getElementById("select_country").value;
+    		var state =document.getElementById("select_state").value;
+		if(country==="0") {
+			document.getElementById("select_state").disabled='disabled';
+			document.getElementById("select_city").disabled='disabled';
+		}  
+		if (country!=="0"){
+			document.getElementById("select_state").disabled=false;	
+		}
+		if(state.disabled===false && state.value!==null && state.value!=="0") {
+			document.getElementById("select_city").disabled=false;
+		}
+		else {
+			document.getElementById("select_city").disabled=true;
+			document.getElementById("select_city").value="";
+		}
+	}
+	hideState(); 
+
+/** Fetches states belonging to selected  country */
         var countryId = $(this).val();
         $.ajax({
             url: CheckState,
@@ -16,9 +37,15 @@ $(document).ready(function() {
                             "state": 0
                         },
                         success: function(cities) {
-                            $("#select_city").html(cities);
-                           $("#select_state").empty(); 
-                        }
+		var state=$("#select_state"); 
+		if(state.val()!=="0" && state.val()!==null && state.prop('disabled')===false)
+              $("#select_city").html(cities);
+		else  
+              $("#select_city").empty();
+        $("#select_state").empty(); 
+                      
+			 },
+			
                     });
                 } else if (statecheck === true) {
                     $.ajax({
@@ -29,11 +56,22 @@ $(document).ready(function() {
                         success: function(states) {
                             $("#select_state").html(states);
                             $("#select_city").empty();
+				 
                         }
                     });
                 }
             }
-        });
+        }); 
     });
+
 });
+
+
+
+
+
+
+
+
+
 
