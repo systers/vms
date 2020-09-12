614b9d0c87af1f9a4f5f7ca2e7fd8bb3486ad186
vaibhavarora102 <aroravaibhav102@gmail.com>
2020-09-13 01:46:40 +0530

    allowed special characters in adddress field
    updated regex

--- vms/administrator/models.py
+++ vms/administrator/models.py
@@ -21,7 +21,7 @@ class Administrator(models.Model):
     address = models.CharField(
         max_length=75,
         validators=[
-            RegexValidator(r'^[(A-Z)|(a-z)|(0-9)|(\s)|(\-)|(\.)|(,)|(\:)]+$', ),
+            RegexValidator(r'^[A-Za-z0-9\s\-.,_/:]+$', ),
         ],
     )
     city = models.ForeignKey(City, null=True, blank=True, on_delete=models.CASCADE)
