89edb6557332f08c247c4bac3e8ca65d5befe133
Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
2020-09-14 14:01:15 +0530

    chore: Form updated

--- vms/registration/forms.py
+++ vms/registration/forms.py
@@ -1,6 +1,7 @@
 # Django
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
 
@@ -33,3 +34,7 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'password')
 
+class SignUpForm(UserCreationForm):  
+        class Meta:  
+            model = User  
+            fields = ('email', 'first_name', 'last_name', 'username')
