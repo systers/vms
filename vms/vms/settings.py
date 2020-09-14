d19cb9b35ec4dcea3a3fc59a881a5378350a6391
Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
2020-09-14 22:24:49 +0530

    Update settings.py

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -129,10 +129,15 @@ STATIC_ROOT = './static/'
 
 # Instead of sending out real email, during development the emails will be sent
 # to stdout, where from they can be inspected.
+
+# if DEBUG:
+#     EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+#     EMAIL_PORT = config('EMAIL_PORT', default=1025, cast=int)
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Khushi17@'  
-EMAIL_HOST_USER = 'dpskhu13108@gmail.com'  
+EMAIL_HOST_PASSWORD = 'your_password'  
+EMAIL_HOST_USER = 'your_email'  
 EMAIL_PORT = 587
 EMAIL_USE_TSL = True  
 
